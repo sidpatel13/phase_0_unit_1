d7546375bab9b851684300c88f09de3989de3880
Sid Patel <sidpatel13@gmail.com>
2014-04-20 21:56:57 -0400

    Update my_solution.js

--- week_3/4_design_code_combat_mission/my_solution.js
+++ week_3/4_design_code_combat_mission/my_solution.js
@@ -1,22 +1,46 @@
 // I worked [by myself, with:] on this challenge
 
 // Your mission description:
-// 
-// 
-// 
+// Kill the evil monkey
+// Rescue the Princess
+// Bring her back to the castle
 
 // Pseudocode
+// move towards evil monkey
+// dodge attack
+// attack!
 // 
-// 
-// 
-// 
-// 
+// ask princess('Are you ok?')
+// ask princess ('Follow me')
+//
+//
+// walk back to the castle
+// say('goodbye princess, nice rescuing you')
+
 
 // Initial Code
+//Part 1 of code
+this.moveTowardsMonkey();
 
+function Dodge(monkeyAttack) {
+	if (monkeyAttack = true) {
+		this.moveLeft();
+	}
+};
 
+this.moveTowardsMonkey();
+this.attackNearbyEnemy();
 
+//Part 2 of code
 
+this.moveTowardsPrincess();
+this.say('Are you ok?');
+this.say('Follow me!');
+
+//Part 3 of code
+
+this.moveTowardsCastle();
+this.say('goodbye princes, nice rescuing you!')
 
 
 // Refactored Code
